69428a060339616918cf989d2772c513d8429381
hendrix <leandrotully@gmail.com>
2023-10-07 17:11:36 -0300

    Iniciando com Express

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-import http from "http"
+console.clear()
+import app from "./src/app.js"
+const PORT = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-type": "text/plain" })
-    res.end("Welcome to Server")
-})
-
-server.listen(3000, () => {
-    console.log(`Server is running at ${3000}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
 })
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to Express")
+})
+
+export default app
