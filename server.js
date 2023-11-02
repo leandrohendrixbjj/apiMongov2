b5a19b42195fdb4f8323bd78e7c067956739e8ee
hendrix <leandrotully@gmail.com>
2023-11-02 09:45:54 -0300

    Conectando MongoDB e API

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 console.clear()
+import confirmConnection from './src/config/confirmConnection.js'
 import app from "./src/app.js"
 const PORT = process.env.PORT || 3000
+
+if (!confirmConnection) {
+    console.log("Error on database connection");
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
---
+++ src/config/confirmConnection.js
@@ -0,0 +1,14 @@
+import dbConnect from './dbConnect.js';
+
+try {
+    
+    dbConnect.on("error", console.log.bind(console, 'Error on database connection'));
+    
+    dbConnect.once("open", () => {
+        console.log('Connection was made successfully');
+    });
+} catch (err) {
+    console.log(`Error on connect: ${err}`);
+}
+
+export default dbConnect;
